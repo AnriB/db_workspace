2cdea17c65dd06a472bb6049f8c2d9b33e83c1f0
AnriB <anribardhi25@gmail.com>
2025-11-23 19:35:31 +0100

    workflow tests

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "workspace_id" {
+  value = azurerm_databricks_workspace.workspace.id
+}
+
+output "workspace_url" {
+  value = azurerm_databricks_workspace.workspace.workspace_url
+}
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "workspace_name" {
+  description = "The name of the Databricks workspace."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the Databricks workspace."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure location where the Databricks workspace will be created."
+  type        = string
+}
+
+variable "databricks_sku" {
+  description = "The SKU of the Databricks workspace."
+  type        = string
+default = "value"
+}
+
+variable "managed_resource_group_name" {
+  description = "The name of the managed resource group for the Databricks workspace."
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the Databricks workspace."
+  type        = map(string)
+  default     = {}  
+}
